9fc991e2920e3cdc39cca5e55f3f3a18a54c3219
Chris van der Geer <chris@MacBook-Air-van-Chris.local>
2017-05-11 23:33:51 +0200

    initial commit

---
+++ src/main/java/nl/cge/sample/MySampleFixture.java
@@ -0,0 +1,11 @@
+package nl.cge.sample;
+
+/**
+ * Created by chris on 11-05-17.
+ */
+public class MySampleFixture {
+
+    public Integer berekenPlus(Integer x, Integer y) {
+        return x + y;
+    }
+}
